be81780e20535eb38a0bea31309a3abbde62cad2
Horizonnns <for.office.gthb@gmail.com>
2024-03-02 00:32:17 +0500

    Made a layout for the pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
-import Main from './components/Main.vue';
+import Header from './components/Header.vue';
+import Main from './pages/MainPage.vue';
+import Tasks from './pages/TasksPage.vue';
+
+import { useRoute } from 'vue-router';
+const route = useRoute();
 </script>
 
 <template>
-	<Main />
+	<div class="space-y-4">
+		<Header />
+
+		<div class="container">
+			<Main v-if="route.path === '/'" />
+
+			<Tasks v-if="route.path === '/tasks'" />
+		</div>
+	</div>
 </template>
