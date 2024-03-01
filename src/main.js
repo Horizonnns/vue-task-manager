5e34de2fa9d870efc3b952013abe0357f42545fd
Horizonnns <for.office.gthb@gmail.com>
2024-03-02 00:21:29 +0500

    Installed and configured the bootstrap package

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+// Import our custom CSS
+import './assets/scss/main.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
 import { createApp } from 'vue';
 import './assets/css/main.css';
 import App from './App.vue';
