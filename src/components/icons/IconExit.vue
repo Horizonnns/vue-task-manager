7a999a30eb2f6194a3f36008d52a0bd6fa61e721
Horizonnns <for.office.gthb@gmail.com>
2024-03-03 04:41:48 +0500

    Added a new "exit" icon to icons

---
+++ src/components/icons/IconExit.vue
@@ -0,0 +1,18 @@
+<template>
+	<svg
+		class="w-9 h-9 cursor-pointer hover:bg-gray-100 active:bg-gray-200 rounded-full p-1.5 duration-200"
+		data-slot="icon"
+		aria-hidden="true"
+		fill="none"
+		stroke-width="2"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M6 18 18 6M6 6l12 12"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		></path>
+	</svg>
+</template>
