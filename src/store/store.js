ab076d1b7422c5dc2fe1c6d475b4f20bef36a169
Horizonnns <for.office.gthb@gmail.com>
2024-03-02 16:57:36 +0500

    Made logic for storing and retrieving tasks from localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,35 @@
-import { createPinia, defineStore } from 'pinia';
+import { defineStore } from 'pinia';
 
 export const useTaskStore = defineStore({
 	id: 'taskManager',
-	state: () => ({ tasks: [] }),
+	state: () => ({ tasks: [], nextId: 1 }),
 	actions: {
+		// adding task
 		addTask(task) {
+			task.id = this.nextId++;
+
 			const newTask = {
 				...task,
-				created_at: Date.now(),
 			};
+
 			this.tasks.push(newTask);
+			this.saveTasksToLocalStorage();
+		},
+
+		// save to localStorage tasks
+		saveTasksToLocalStorage() {
+			localStorage.setItem('tasks', JSON.stringify(this.tasks));
+			localStorage.setItem('nextId', this.nextId);
+		},
+
+		// get from localStorage tasks
+		loadTasksFromLocalStorage() {
+			const tasksFromLocalStorage = JSON.parse(
+				localStorage.getItem('tasks') || '[]'
+			);
+			this.tasks = tasksFromLocalStorage;
+
+			this.nextId = parseInt(localStorage.getItem('nextId')) || 1;
 		},
 	},
 });
-
-export const pinia = createPinia();
